e86ea6aa3f7e1cc77f69bb46546c2989fc4f80f6
Shoe Gazer <shoegazer1969@outlook.com>
2024-03-25 16:47:33 -0700

    add ethers

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
+import * as ethers from "ethers";
 import * as dotenv from "dotenv";
 dotenv.config();
+
+            const wallet = ethers.Wallet.createRandom();
+            console.log(`MNEMONIC PHRASE: ${wallet.mnemonic.phrase}`);
 
 console.log("Hello, Node Typescript app")
 console.log("MESSAGE: " + process.env.MESSAGE)
